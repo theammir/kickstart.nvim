5bb6894b0c4c446ceb0c0ce7277741c41725c8ad
theammir <the@ammir.dev>
2025-01-14 10:19:34 +0200

    fix(mini.pairs): `<>` pairing for shift operators

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -22,12 +22,12 @@ return {
           ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
           ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
           ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
-          ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^%s\\].', register = { cr = false } },
+          ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^%s<\\].', register = { cr = false } },
 
           [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
           [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
           ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
-          ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^%s\\].', register = { cr = false } },
+          ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^%s>\\].', register = { cr = false } },
 
           ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
           ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
