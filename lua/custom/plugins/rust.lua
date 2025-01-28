f5f589b872e63d9d3e01a01720dd8684adbcc9c3
theammir <the@ammir.dev>
2025-01-28 03:23:27 +0200

    feat: language-specific settings

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -7,10 +7,11 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(args)
-          vim.keymap.set('n', '<localleader>le', '<cmd>RustLsp explainError<cr>', { desc = 'Explain error on this line' })
-          vim.keymap.set('n', '<localleader>lE', '<cmd>RustLsp expandMacro<cr>', { desc = 'Expand Rust macro' })
           vim.keymap.set('n', '<localleader>lc', '<cmd>RustLsp openCargo<cr>', { desc = 'Open current Cargo.toml' })
           vim.keymap.set('n', '<localleader>ld', '<cmd>RustLsp openDocs<cr>', { desc = 'View symbol at docs.rs' })
+          vim.keymap.set('n', '<localleader>le', '<cmd>RustLsp explainError<cr>', { desc = 'Explain error on this line' })
+          vim.keymap.set('n', '<localleader>lE', '<cmd>RustLsp expandMacro<cr>', { desc = 'Expand Rust macro' })
+          vim.keymap.set('n', '<localleader>lt', '<cmd>RustTest<cr>', { desc = 'Run test under cursor' })
         end,
       })
       vim.g.rustaceanvim = {
